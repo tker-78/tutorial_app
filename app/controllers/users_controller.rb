37a7bca5e905e7a3ca993ec262b6cf910264c9b9
takuya kinoshita <kktak02@gmail.com>
2022-02-16 07:25:04 +0900

    finished section 7

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,10 +5,26 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "ユーザー登録が成功しました"
+      redirect_to @user
+    else
+      flash[:danger] = "ユーザー登録に失敗しました"
+      render 'new'
+    end
   end
 
   def show
     @user = User.find(params[:id])
   end
 
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
---
+++ spec/features/user_signup_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+RSpec.feature "Users", type: :feature do
+  scenario "create new user" do
+    expect {
+      visit root_path
+      click_link "Sign up now"
+      fill_in "Name", with: "tatataa"
+      fill_in "Email", with: "tatat@gmail.com"
+      fill_in "Password", with: "password"
+      fill_in "Confirmation", with: "password"
+
+      click_button "Create my account"
+      expect(page).to have_http_status(200)
+      expect(page).to have_content("ユーザー登録が成功しました")
+      expect(page).to have_content "This is the show page of user tatataa"
+          }.to change(User.all, :count).by(1) 
+  end
+
+  scenario "fail to create new user" do
+    visit root_path
+    click_link "Sign up now"
+    fill_in "Name", with: ""
+    fill_in "Email", with: ""
+    fill_in "Password", with: ""
+    fill_in "Confirmation", with: ""
+
+    click_button "Create my account"
+    expect(page).to have_content("ユーザー登録に失敗しました")
+    expect(page).to have_content "The form contains 6 errors"
+  end
+
+end
