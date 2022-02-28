9598915dbf4ade647baf24de1930d7d28f98f65d
takuya kinoshita <kktak02@gmail.com>
2022-03-01 06:15:37 +0900

    modify error

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,11 +69,11 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
 
-  host = '<https://obscure-taiga-77545>.herokuapp.com'
+  host = '<obscure-taiga-77545>.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     port: ENV['MAILGUN_SMTP_PORT'],
-    address: ENV['MAIGUN_SMTP_SERVER'],
+    address: ENV['MAILGUN_SMTP_SERVER'],
     user_name: ENV['MAILGUN_SMTP_LOGIN'],
     password: ENV['MAILGUN_SMTP_PASSWORD'],
     domain: host,
