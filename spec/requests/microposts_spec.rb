6834d61ae1df1c3b35d5998738f17d64ccc753b3
takuya kinoshita <kktak02@gmail.com>
2022-03-05 16:40:13 +0900

    finished section 13.45

--- spec/requests/microposts_spec.rb
+++ spec/requests/microposts_spec.rb
@@ -1,7 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe "Microposts", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  before do
+    @user = FactoryBot.create(:user)
+    @micropost = @user.microposts.create(content: "content", user_id: @user.id)
+  end
+  it "when not logged in" do
+    expect { post microposts_path, params: { micropost: { content: "new micropost" }}
+  }.to_not change {Micropost.count}
+  end
+
+  it "when logged in" do
+    log_in_as @user
+    expect { post microposts_path, params: { micropost: { content: "new micropost" }}
+    }.to change {Micropost.count}
+
   end
 end
